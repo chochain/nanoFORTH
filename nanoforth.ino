724c50a81ece91e2c424dcebc092aa4aeb7beb96
gnii <chochain@yahoo.com>
2021-05-08 17:39:54 -0700

    ~/n4.ino add demo tasks (led and onboard blinker)

--- nanoforth.ino
+++ nanoforth.ino
@@ -5,10 +5,40 @@
 
 NanoForth *n4;                 // create a nanoForth instance
 
+N4_FUNC(blink)                 // 
+{
+    N4_BEGIN();
+    
+    digitalWrite(LED_BUILTIN, HIGH);
+    N4_DELAY(500);
+    digitalWrite(LED_BUILTIN, LOW);
+    N4_DELAY(500);
+
+    N4_END();
+}
+
+N4_FUNC(led_toggle)
+{
+    N4_BEGIN();
+
+    digitalWrite(5, HIGH);
+    digitalWrite(6, LOW);
+    N4_DELAY(250);
+    
+    digitalWrite(5, LOW);
+    digitalWrite(6, HIGH);
+    N4_DELAY(250);
+
+    N4_END();
+}
+
 void setup()
 {
     Serial.begin(115200);
-    n4 = new NanoForth();
+    n4 = new NanoForth(0x480, 0x80);
+    
+    n4->add(blink);            // add blink
+    n4->add(led_toggle);
 }
 
 void loop()
@@ -16,11 +46,4 @@ void loop()
     n4->run();                 // execute one vm cycle
 }
 
-void n4_loop()
-{
-    digitalWrite(LED_BUILTIN, HIGH);
-    //n4_delay(500);
-    digitalWrite(LED_BUILTIN, LOW);
-    //n4_delay(500);
-}
 
