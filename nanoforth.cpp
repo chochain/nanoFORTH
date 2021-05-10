3d47156596caba8123b0532fd80f5c5d71065f1d
gnii <chochain@yahoo.com>
2021-05-09 19:55:24 -0700

    ~/nanoforth.cpp add history comments

--- nanoforth.cpp
+++ nanoforth.cpp
@@ -1,36 +1,40 @@
-/// nanoFORTH - Forth for Arduino Nano (and UNO)
-/**
- * 2004-07 T. NAKAGAWA (Tiny FORTH), original reference 
- * 2020-08 circuit4u@medium.com (Tiny Forth NXT), Arduino reference
- * 2021-03 CC Lee: modularize with comments [6698,1006]; add buffer handler[6924,1036]
- * 2021-04-02: CC - add multi-tasker, byte count[6782/1056],
- *    [6930,1056] add threads
- *    [6908,1014] use F()
- *    [6860,852]  use PROGMEM, pgm_read_byte in find()
- *    [6906,852]  getnum support negative number
- *    [7012,860]  add digitalR/W
- *  2021-04-03: CC
- *    [6898,852]  add vm_delay
- *    [6912,852]  update EEPROM IO
- *    [6842,852]  execution tracing enable/disable
- *    [6922,852]  add CELL (CEL), ALLOT (ALO) opcodes
- *  2021-04-16: CC
- *    [7446,932]  add list_word, use 80-byte console buffer
- *    [7396,932]  add ASM_TRACE from EXE_TRACE options
- *    [7676,802]  grow Task at end of heap
- *    [8266,802]  if use array instead of pointer arithmetics, revert!
- *  2021-05-03: CC
- *    [7818,1428] forget Task struct; grow MEM_SZ to DIC_SZ+STK_SZ (1K+64*2)
- *    [8254,1430] add execution tracing option
- *  2021-05-06: CC
- *    [9214,1424] refactor to C++ for Arduino lib
- *    [11750,280] add mem,stk sizing; VM and ASM objects dynamically created
- *    [11956,272] add struct n4_task and N4_FUNC multi-tasker
- */
+///
+/// \file nanoforth.cpp
+/// \brief NanoForth main controller class implementation
+///
+/// Refactor History:
+/// -----------------
+///> 2004-07 T. NAKAGAWA (Tiny FORTH), original reference<p>
+///> 2020-08 circuit4u@medium.com (Tiny Forth NXT), Arduino reference<p>
+///> 2021-03 CC Lee: modularize with comments [6698,1006]; add buffer handler[6924,1036]<p>
+///> 2021-04-02: CC - add multi-tasker, byte count[6782/1056],<p>
+///  * [6930,1056] add threads
+///  * [6908,1014] use F()
+///  * [6860,852]  use PROGMEM, pgm_read_byte in find()
+///  * [6906,852]  getnum support negative number
+///  * [7012,860]  add digitalR/W<p>
+///> 2021-04-03: CC<p>
+///  * [6898,852]  add vm_delay
+///  * [6912,852]  update EEPROM IO
+///  * [6842,852]  execution tracing enable/disable
+///  * [6922,852]  add CELL (CEL), ALLOT (ALO) opcodes<p>
+///> 2021-04-16: CC<p>
+///  * [7446,932]  add list_word, use 80-byte console buffer
+///  * [7396,932]  add ASM_TRACE from EXE_TRACE options
+///  * [7676,802]  grow Task at end of heap
+///  * [8266,802]  if use array instead of pointer arithmetics, revert!<p>
+///> 2021-05-03: CC<p>
+///  * [7818,1428] forget Task struct; grow MEM_SZ to DIC_SZ+STK_SZ (1K+64*2)
+///  * [8254,1430] add execution tracing option<p>
+///> 2021-05-06: CC<p>
+///  * [9214,1424] refactor to C++ for Arduino lib
+///  * [11750,280] add mem,stk sizing; VM and ASM objects dynamically created
+///  * [11956,272] add struct n4_task and N4_FUNC multi-tasker<p>
+///
 #include "nanoforth_vm.h"
-///
-/// user function linked-list
-///
+//
+// user function linked-list
+//
 static n4_tptr _n4tsk = 0;
 static N4VM    *_n4vm  = new N4VM();
 ///
@@ -44,7 +48,7 @@ void NanoForth::begin(U16 mem_sz, U16 stk_sz)
     _n4vm->info();
 }
 ///
-/// single step for Arduino loop
+/// * single step for Arduino loop
 ///
 void NanoForth::add(void (*ufunc)(n4_tptr))
 {
@@ -88,9 +92,9 @@ void NanoForth::wait(U32 ms)
     U32 t = millis() + ms;
     while (millis()<t) yield();
 }
-///
-/// for Eclipse debugging
-///
+//
+// for Eclipse debugging
+//
 /*
 int main(int argc, char **argv)
 {
