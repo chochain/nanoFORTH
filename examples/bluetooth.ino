94030f53a52650b8e24fc8c7850dc8eada7255b2
gnii <chochain@yahoo.com>
2021-06-20 16:37:08 -0700

    ~/examples update bluetooth

--- examples/bluetooth.ino
+++ examples/bluetooth.ino
@@ -1,10 +1,13 @@
 //
-// nanoFORTH - Forth for Arduino Nano (and UNO)
+// nanoFORTH - with Bluetooth HC-05
 //
-#include <SoftwareSerial.h>
+#include <AltSoftSerial.h>
 #include "src/nanoforth.h"
 
-N4_TASK(blink)                    ///< create blinking task (i.e. built-in LED on pin 13)
+AltSoftSerial bt;                   ///< default: RX on pin 8, TX on pin 9
+NanoForth     n4;                   ///< create NanoForth instance
+
+N4_TASK(blink)                      ///< create blinking task (i.e. built-in LED on pin 13)
 {
     digitalWrite(LED_BUILTIN, HIGH);
     N4_DELAY(500);
@@ -13,25 +16,25 @@ N4_TASK(blink)                    ///< create blinking task (i.e. built-in LED o
 }
 N4_END;
 
-SoftwareSerial hm11(7, 8);
-NanoForth n4;                     ///< create NanoForth instance
-
 void setup()
 {
-    Serial.begin(115200);         ///< init Serial IO, make sure it is set to 'Both BL & CR' to capture input
-    hm11.begin(57600);
+    while (!Serial);
+    Serial.begin(115200);           ///< init Serial IO, make sure it is set to 'Both BL & CR' to capture input
     
-    if (n4.begin()) {             /// default: (Serial,0x480,0x80), try reducing if memory is constrained
+    bt.begin(9600);                 ///< setup Bluetooth as serial device
+    delay(1000);
+    
+    if (n4.begin(bt)) {             /// default: (Serial,1,0x480,0x80), try reducing if memory is constrained
         Serial.print(F("ERROR: memory allocation failed!"));
     }
-    n4.add(blink);                ///< add blink task to NanoForth task manager
-
+    n4.add(blink);                  ///< add blink task to NanoForth task manager
+    
     pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop()
 {
-    n4.step();                   // execute one nanoForth VM cycle
+    n4.exec();                      // execute one nanoForth command line from serial console
 }
 
 
