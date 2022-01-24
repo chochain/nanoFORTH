cc0987816359061fdde4b2a99aeaa1055c36b221
gnii <chochain@yahoo.com>
2022-01-24 15:46:39 +0800

    ~/ use examples/0_blink as sample

--- nanoforth.ino
+++ nanoforth.ino
@@ -1,13 +1,23 @@
-//
-// nanoFORTH - with Bluetooth HC-05
-//
-#include <AltSoftSerial.h>
-#include "src/nanoforth.h"
+/**
+ *  @file examples/0_blink.ino
+ *  @brief nanoFORTH example - Blink pin 13
+ *
+ *  Our first Sketch - demostrates nanoFORTH support multi-tasking
+ *  + a user task that blinks built-in pin 13
+ *  + nanoFORTH itself runs in parallel
+ *
+ *  open Serial Monitor as the console input to nanoFORTH
+ *  + baud rate set to 115200
+ *  + line ending set to Both NL & CR
+ *
+ *  Once compiled/uploaded, you should see
+ *  + some nanoFORTH init system info
+ *  + ok prompt
+ *  + try type WRD and hit return on the input above
+ */
+#include "nanoforth.h"
 
-AltSoftSerial bt;                   ///< default: RX on pin 8, TX on pin 9
-NanoForth     n4;                   ///< create NanoForth instance
-
-N4_TASK(blink)                      ///< create blinking task (i.e. built-in LED on pin 13)
+N4_TASK(blink)                    ///< create blinking task (i.e. built-in LED on pin 13)
 {
     digitalWrite(LED_BUILTIN, HIGH);
     N4_DELAY(500);
@@ -16,25 +26,22 @@ N4_TASK(blink)                      ///< create blinking task (i.e. built-in LED
 }
 N4_END;
 
+NanoForth n4;                     ///< create NanoForth instance
 void setup()
 {
-    while (!Serial);
-    Serial.begin(115200);           ///< init Serial IO, make sure it is set to 'Both BL & CR' to capture input
+    Serial.begin(115200);         ///< init Serial IO, make sure it is set to 'Both BL & CR' to capture input
     
-    bt.begin(9600);                 ///< setup Bluetooth as serial device
-    delay(1000);
-    
-    if (n4.begin(bt)) {             /// default: (Serial,1,0x480,0x80), try reducing if memory is constrained
+    if (n4.begin()) {             /// default: (Serial,0x480,0x80), try reducing if memory is constrained
         Serial.print(F("ERROR: memory allocation failed!"));
     }
-    n4.add(blink);                  ///< add blink task to NanoForth task manager
-    
+    n4.add(blink);                ///< add blink task to NanoForth task manager
+
     pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop()
 {
-    n4.exec();                      // execute one nanoForth command line from serial console
+    n4.exec();                   // execute one nanoForth VM cycle
 }
 
 
