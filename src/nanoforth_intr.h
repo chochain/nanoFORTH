61d9cc49b52a6ca827aa133f16203a7e95c6f763
gnii <chochain@yahoo.com>
2023-01-05 19:41:20 +0800

    ~/src/nanoforth_intr.h make member volatile

--- src/nanoforth_intr.h
+++ src/nanoforth_intr.h
@@ -16,18 +16,18 @@
 
 typedef void (N4VM::* FPTR)(U16);
 struct N4Intr {
-	static U8  t_idx;            ///< timer ISR index
-	static U8  t_hit;            ///< 8-bit for 8 timer ISR vectors
-	static U8  p_hit;            ///< pin change interrupt
-	static U8  xxx;              ///< reserved
+    volatile static U8  t_hit;     ///< 8-bit for 8 timer ISR vectors
+    volatile static U8  p_hit;     ///< pin change interrupt
+    volatile static U16 t_cnt[8];  ///< timer counter
 
-    static U16  p_xt[3];         ///< pin change ISR
-    static U16  t_cnt[8];        ///< timer counter
-    static U16  t_max[8];        ///< timer CTC value
-    static U16  t_xt[8];         ///< timer ISR
+    static U8   t_idx;             ///< timer ISR index
+    static U8   xxx;               ///< reserved
+    static U16  p_xt[3];           ///< pin change ISR
+    static U16  t_max[8];          ///< timer CTC value
+    static U16  t_xt[8];           ///< timer ISR
 
-    static void reset();         ///< reset interrupts
-    static U16  hits(U16 *xt);   ///< fetch interrupt service routines
+    static void reset();           ///< reset interrupts
+    static U16  hits(U16 *xt);     ///< fetch interrupt service routines
 
     static void add_timer(U16 prd, U16 xt);
 #if ARDUINO
@@ -37,7 +37,7 @@ struct N4Intr {
 #else // !ARDUINO (mocked interrupt handlers)
     static void add_pci(U16 pin, U16 xt) {}
     static void enable_timer(U16 f)      {}
-    static void enable_pci(U16 f)  		 {}
+    static void enable_pci(U16 f)           {}
 #endif
 };
 #endif //__SRC_NANOFORTH_INTR_H
