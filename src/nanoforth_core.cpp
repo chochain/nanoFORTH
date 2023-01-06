00b8836c7f74a24372fa9a22532ac6896a7275ab
gnii <chochain@yahoo.com>
2023-01-06 14:37:31 +0800

    ~/src/nanoforth_core replace N4Core static class as namespace

--- src/nanoforth_core.cpp
+++ src/nanoforth_core.cpp
@@ -1,24 +1,45 @@
 /**
- * @file nanoforth_core.cpp
- * @brief nanoForth Core Utility abstract class implementation
+ * @file
+ * @brief nanoForth Core Utilities
  *
  */
 #include "nanoforth_core.h"
+
+#if !ARDUINO
+int  Serial;           				     ///< fake serial interface
+#endif // !ARDUINO
+
+namespace N4Core {
 ///
 ///@name VM static variables
 ///@{
-U8     *N4Core::dic    { NULL };               ///< base of dictionary
-U16    *N4Core::rp     { NULL };               ///< base of return stack
-S16    *N4Core::sp     { NULL };               ///< top of data stack
-U8     *N4Core::tib    { NULL };               ///< base of terminal input buffer
+Stream *xio    { &Serial };                    ///< default to Arduino Serial Monitor
+U8     *dic    { NULL };                       ///< base of dictionary
+U16    *rp     { NULL };                       ///< base of return stack
+S16    *sp     { NULL };                       ///< top of data stack
+U8     *tib    { NULL };                       ///< base of terminal input buffer
 ///@}
 ///@name IO controls
 ///@{
-Stream *N4Core::_io    { &Serial };            ///< default to Arduino Serial Monitor
-U8      N4Core::_hex   { 0 };                  ///< numeric radix for display
-U8      N4Core::_empty { 1 };                  ///< empty flag for terminal input buffer
-U8      N4Core::_ucase { 1 };                  ///< empty flag for terminal input buffer
-U8      N4Core::_trc   { 0 };                  ///< tracing flag for debug output
+U8      _hex   { 0 };                          ///< numeric radix for display
+U8      _empty { 1 };                          ///< empty flag for terminal input buffer
+U8      _ucase { 1 };                          ///< empty flag for terminal input buffer
+U8      _trc   { 0 };                          ///< tracing flag for debug output
+
+void set_mem(U8 *mem, U16 msz, U16 ssz) {
+    dic = mem;                                 /// * start of dictionary
+    rp  = (U16*)(mem+msz);                     /// * grows toward sp
+    sp  = (S16*)(mem+msz+ssz);                 /// * grows toward 0
+    tib = (U8*)sp;                             /// * grows toward max
+}
+void set_io(Stream *io) { xio  = io; }         ///< initialize or redirect IO stream
+void set_hex(U8 f)      { _hex = f; }          ///< enable/disable hex numeric radix
+void set_trace(U8 f)    { _trc = f; }          ///< enable/disable execution tracing
+U8   is_tracing()       { return _trc; }       ///< return tracing flag
+void set_ucase(U8 uc)   { _ucase = uc; }       ///< set case sensitiveness
+char uc(char c)      {                         ///< upper case for case-insensitive matching
+    return (_ucase && (c>='A')) ? c&0x5f : c;
+}
 ///@}
 ///@name Console IO Functions with Cooperative Threading support
 ///@{
@@ -27,37 +48,36 @@ U8      N4Core::_trc   { 0 };                  ///< tracing flag for debug outpu
 ///
 ///> char input from console
 ///
-char N4Core::key()
+char key()
 {
-    while (!_io->available()) NanoForth::yield();
-    return _io->read();
+    while (!xio->available()) NanoForth::yield();  /// TODO: add _isr here
+    return xio->read();
 }
-void N4Core::d_chr(char c)     {
-    _io->print(c);
+void d_chr(char c)     {
+    xio->print(c);
     if (c=='\n') {
-        _io->flush();
+        xio->flush();
         NanoForth::yield();
     }
 }
-void N4Core::d_adr(U16 a)      { d_nib(a>>8); d_nib((a>>4)&0xf); d_nib(a&0xf); }
-void N4Core::d_ptr(U8 *p)      { U16 a=(U16)p; d_chr('p'); d_adr(a); }
-void N4Core::d_num(S16 n)      { _hex ? _io->print(n&0xffff,HEX) : _io->print(n); }
+void d_adr(U16 a)      { d_nib(a>>8); d_nib((a>>4)&0xf); d_nib(a&0xf); }
+void d_ptr(U8 *p)      { U16 a=(U16)p; d_chr('p'); d_adr(a); }
+void d_num(S16 n)      { _hex ? xio->print(n&0xffff,HEX) : xio->print(n); }
 #else
-int  Serial;                   // fake serial interface
-char N4Core::key()             { return getchar();  }
-void N4Core::d_chr(char c)     { printf("%c", c);   }
-void N4Core::d_adr(U16 a)      { printf("%03x", a); }
-void N4Core::d_ptr(U8 *p)      { printf("%p", p);   }
-void N4Core::d_num(S16 n)      { printf(_hex ? "%x" : "%d", n); }
+char key()             { return getchar();  }
+void d_chr(char c)     { printf("%c", c);   }
+void d_adr(U16 a)      { printf("%03x", a); }
+void d_ptr(U8 *p)      { printf("%p", p);   }
+void d_num(S16 n)      { printf(_hex ? "%x" : "%d", n); }
 #endif //ARDUINO
-void N4Core::d_str(U8 *p)      { for (U8 i=0, sz=*p++; i<sz; i++) d_chr(*p++); }
-void N4Core::d_nib(U8 n)       { d_chr((n) + ((n)>9 ? 'a'-10 : '0')); }
-void N4Core::d_u8(U8 c)        { d_nib(c>>4); d_nib(c&0xf); }
+void d_str(U8 *p)      { for (U8 i=0, sz=*p++; i<sz; i++) d_chr(*p++); }
+void d_nib(U8 n)       { d_chr((n) + ((n)>9 ? 'a'-10 : '0')); }
+void d_u8(U8 c)        { d_nib(c>>4); d_nib(c&0xf); }
 ///@}
 ///
 ///> dump byte-stream between pointers with delimiter option
 ///
-void N4Core::d_mem(U8* base, U8 *p0, U16 sz, U8 delim)
+void d_mem(U8* base, U8 *p0, U16 sz, U8 delim)
 {
     d_adr((U16)(p0 - base)); d_chr(':');
     for (int n=0; n<sz; n++) {
@@ -69,7 +89,7 @@ void N4Core::d_mem(U8* base, U8 *p0, U16 sz, U8 delim)
 ///
 ///> display the opcode name
 ///
-void N4Core::d_name(U8 op, const char *lst, U8 space)
+void d_name(U8 op, const char *lst, U8 space)
 {
 #if ARDUINO
     PGM_P p = reinterpret_cast<PGM_P>(lst)+1+op*3;
@@ -84,7 +104,7 @@ void N4Core::d_name(U8 op, const char *lst, U8 space)
 ///
 ///> parse a literal from string
 ///
-U8 N4Core::number(U8 *str, S16 *num)
+U8 number(U8 *str, S16 *num)
 {
     S16 n   = 0;
     U8  neg = (*str=='-') ? (str++, 1)  : 0;  /// * handle negative sign
@@ -102,20 +122,50 @@ U8 N4Core::number(U8 *str, S16 *num)
 ///
 ///> check whether token available in input buffer
 ///
-U8 N4Core::is_tib_empty()
+U8 is_tib_empty()
 {
     return _empty;
 }
 ///
 ///> clear terminal input buffer
 ///
-void N4Core::clear_tib() {
+void clear_tib() {
     get_token(true);                         ///> empty the static tib inside #get_token
+}
+///
+///> fill input buffer from console char-by-char til CR or LF hit
+///
+void _console_input()
+{
+    U8 *p = tib;
+    d_chr('\n');
+    for (;;) {
+        char c = key();                      /// * get one char from input stream
+        if (c=='\r' || c=='\n') {            /// * split on RETURN
+            if (p > tib) {
+                *p     = ' ';                /// * pad extra space (in case word is 1-char)
+                *(p+1) = 0;                  /// * terminate input string
+                break;                       /// * skip empty token
+            }
+        }
+        else if (c=='\b' && p > tib) {       /// * backspace
+            *(--p) = ' ';
+            d_chr(' ');
+            d_chr('\b');
+        }
+        else if (p > (U8*)(&c - 0x10)) {     /// * address of auto variable
+            show("TIB!\n");
+            *p = 0;
+            break;
+        }
+        else *p++ = c;
+    }
+    _empty = (p==tib);
 }
 ///
 ///> capture a token from console input buffer
 ///
-U8 *N4Core::get_token(bool rst)
+U8 *get_token(bool rst)
 {
     static U8 *tp = tib;                     ///> token pointer to input buffer
     static U8 dq  = 0;                       ///> dot_string flag
@@ -152,7 +202,7 @@ U8 *N4Core::get_token(bool rst)
 ///> search keyword in a nanoForth name field list
 ///  * one blank byte padded at the end of input string
 ///
-U8 N4Core::scan(U8 *tkn, const char *lst, U16 *id)
+U8 scan(U8 *tkn, const char *lst, U16 *id)
 {
     for (int n=1, m=pgm_read_byte(lst); n < m*3; n+=3) {
         if (uc(tkn[0])==pgm_read_byte(lst+n)   &&
@@ -164,33 +214,4 @@ U8 N4Core::scan(U8 *tkn, const char *lst, U16 *id)
     }
     return 0;
 }
-///
-///> fill input buffer from console char-by-char til CR or LF hit
-///
-void N4Core::_console_input()
-{
-    U8 *p = tib;
-    d_chr('\n');
-    for (;;) {
-        char c = key();                      // get one char from input stream
-        if (c=='\r' || c=='\n') {            // split on RETURN
-            if (p > tib) {
-                *p     = ' ';                // pad extra space (in case word is 1-char)
-                *(p+1) = 0;                  // terminate input string
-                break;                       // skip empty token
-            }
-        }
-        else if (c=='\b' && p > tib) {       // backspace
-            *(--p) = ' ';
-            d_chr(' ');
-            d_chr('\b');
-        }
-        else if (p > (U8*)(&c - 0x10)) {     // address of auto variable
-            show("TIB!\n");
-            *p = 0;
-            break;
-        }
-        else *p++ = c;
-    }
-    _empty = (p==tib);
-}
+} // namespace N4Core
--- src/nanoforth_core.h
+++ src/nanoforth_core.h
@@ -1,13 +1,13 @@
 /**
- * @file nanoforth_core.h
- * @brief nanoForth Core Utility abstract class
+ * @file
+ * @brief nanoForth Core Utilities
  */
 #ifndef __SRC_NANOFORTH_CORE_H
 #define __SRC_NANOFORTH_CORE_H
 #include "nanoforth.h"
 
 #if ARDUINO
-#define show(s)      { _io->print(F(s)); _io->flush(); }
+#define show(s)      { xio->print(F(s)); xio->flush(); }
 #define INLINE       __attribute__((always_inline))
 #else
 #define show(s)      log(s)
@@ -30,79 +30,62 @@
 ///
 /// nanoForth Core Helper abstract class
 ///
-class N4Core
+namespace N4Core
 {
-    static U8   _empty;                    ///< token input buffer empty flag
-    static U8   _ucase;                    ///< case insensitive
-    static U8   _hex;                      ///< numeric radix for display
-    static U8   _trc;                      ///< tracing flags
+    extern Stream *xio;             ///< default to Arduino Serial Monitor
+    extern U8     *dic;             ///< base of dictionary
+    extern U16    *rp;				///< base of return stack
+    extern S16    *sp;              ///< top of data stack
+    extern U8     *tib;             ///< base of terminal input buffer
 
-protected:
-    static U8     *dic;                    ///< base of dictionary
-    static U16    *rp;					   ///< base of return stack
-    static S16    *sp;                     ///< top of data stack
-    static U8     *tib;                    ///< base of terminal input buffer
-    static Stream *_io;                    ///< IO stream (static member)
-
-public:
-    static void set_mem(U8 *mem, U16 msz, U16 ssz) {
-        dic = mem;                         /// * start of dictionary
-        rp  = (U16*)(mem+msz);             /// * grows toward sp
-        sp  = (S16*)(mem+msz+ssz);         /// * grows toward 0
-        tib = (U8*)sp;                     /// * grows toward max
-    }
-    static void set_io(Stream *io) { _io = io; }    ///< initialize or redirect IO stream
-    static void set_hex(U8 f)      { _hex = f; }    ///< enable/disable hex numeric radix
-    static void set_trace(U8 f)    { _trc = f; }    ///< enable/disable execution tracing
-    static U8   is_tracing()       { return _trc; } ///< return tracing flag
-    static void set_ucase(U8 uc)   { _ucase = uc; } ///< set case sensitiveness
-    static char uc(char c)         {                ///< upper case for case-insensitive matching
-        return (_ucase && (c>='A')) ? c&0x5f : c;
-    }
+    void set_mem(U8 *mem, U16 msz, U16 ssz);
+    void set_io(Stream *io);        ///< initialize or redirect IO stream
+    void set_hex(U8 f);             ///< enable/disable hex numeric radix
+    void set_trace(U8 f);           ///< enable/disable execution tracing
+    U8   is_tracing();              ///< return tracing flag
+    void set_ucase(U8 uc);          ///< set case sensitiveness
+    char uc(char c);
     ///
     ///@name dot_* for Console Input/Output Routines
     ///@{
-    static char key();                     ///< Arduino's Serial.getchar(), yield to user tasks when waiting
-    static void d_chr(char c);             ///< print a char to console
-    static void d_adr(U16 a);              ///< print a 12-bit address
-    static void d_str(U8 *p);              ///< handle dot string (byte-stream leading with length)
-    static void d_ptr(U8 *p);              ///< print a pointer
-    static void d_nib(U8 n);               ///< print a nibble
-    static void d_u8(U8 c);                ///< print a 8-bit hex number
-    static void d_num(S16 n);              ///< sent a number literal to console
-    static void d_mem(                     ///< display memory block
-        U8 *base,                          ///< reference memory pointer (start of dictionary)<br/>
-        U8 *p0,                            ///< starting memory pointer<br/>
-        U16 sz,                            ///< number of bytes to print<br/>
-        U8 delim                           ///< delimiter, ' ' for space, 0 for none
+    char key();                     ///< Arduino's Serial.getchar(), yield to user tasks when waiting
+    void d_chr(char c);             ///< print a char to console
+    void d_adr(U16 a);              ///< print a 12-bit address
+    void d_str(U8 *p);              ///< handle dot string (byte-stream leading with length)
+    void d_ptr(U8 *p);              ///< print a pointer
+    void d_nib(U8 n);               ///< print a nibble
+    void d_u8(U8 c);                ///< print a 8-bit hex number
+    void d_num(S16 n);              ///< sent a number literal to console
+    void d_mem(                     ///< display memory block
+        U8 *base,                   ///< reference memory pointer (start of dictionary)<br/>
+        U8 *p0,                     ///< starting memory pointer<br/>
+        U16 sz,                     ///< number of bytes to print<br/>
+        U8 delim                    ///< delimiter, ' ' for space, 0 for none
         );
-    static void d_name(                    ///< display opcode 3-char name
-        U8 op,                             ///< opcode
-        const char *lst,                   ///< nanoForth string formatted list
-        U8 space                           ///< delimiter to append at the end
+    void d_name(                    ///< display opcode 3-char name
+        U8 op,                      ///< opcode
+        const char *lst,            ///< nanoForth string formatted list
+        U8 space                    ///< delimiter to append at the end
         );
     ///@}
     ///
     ///@name Search Functions
     ///@{
-    static U8   is_tib_empty();            ///< check whether input buffer is empty
-    static void clear_tib();               ///< reset input buffer
-    static U8   *get_token(bool rst=false);///< get a token from console input
-    static U8   number(                    ///< process a literal from string given
-        U8 *tkn,                           ///< token string of a number
-        S16 *num                           ///< number pointer for return value
+    U8   is_tib_empty();            ///< check whether input buffer is empty
+    void clear_tib();               ///< reset input buffer
+    U8   *get_token(bool rst=false);///< get a token from console input
+    U8   number(                    ///< process a literal from string given
+        U8 *tkn,                    ///< token string of a number
+        S16 *num                    ///< number pointer for return value
         );
     ///
     /// scan token from a given string list
     ///
-    static U8   scan(
-        U8 *tkn,                           ///< token to be searched
-        const char *lst,                   ///< string list to be scanned
-        U16 *id                            ///< resultant index if found
+    U8   scan(
+        U8 *tkn,                    ///< token to be searched
+        const char *lst,            ///< string list to be scanned
+        U16 *id                     ///< resultant index if found
         );
     ///@}
-
-private:
-    static void _console_input();          ///< retrieve input stream from console
 };
 #endif //__SRC_NANOFORTH_CORE_H
