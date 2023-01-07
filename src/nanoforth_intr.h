9696845e9c8daa4ad834994b773cc9f05f67a0cb
gnii <chochain@yahoo.com>
2023-01-07 13:58:18 +0800

    ~/src/nanoforth_intr change N4Intr into namespace

--- src/nanoforth_intr.h
+++ src/nanoforth_intr.h
@@ -19,29 +19,18 @@
 #define SEI()
 #endif // ARDUINO
 
-struct N4Intr {
-    volatile static U8  t_hit;     ///< 8-bit for 8 timer ISR vectors
-    volatile static U8  p_hit;     ///< pin change interrupt
-    volatile static U16 t_cnt[8];  ///< timer counter
+namespace N4Intr {
+	extern U8   t_idx;             ///< timer ISR index
+	extern U16  p_xt[3];           ///< pin change ISR
+	extern U16  t_xt[8];           ///< timer ISR
 
-    static U8   t_idx;             ///< timer ISR index
-    static U8   xxx;               ///< reserved
-    static U16  p_xt[3];           ///< pin change ISR
-    static U16  t_max[8];          ///< timer CTC value
-    static U16  t_xt[8];           ///< timer ISR
+    void reset();                  ///< reset interrupts
+    U16  hits();                   ///< fetch interrupt service routines
 
-    static void reset();           ///< reset interrupts
-    static U16  hits();            ///< fetch interrupt service routines
+    void add_timer(U16 prd, U16 xt);
+    void add_pci(U16 pin, U16 xt);
+    void enable_timer(U16 f);
+    void enable_pci(U16 f);
+};    // namespace N4Intr
 
-    static void add_timer(U16 prd, U16 xt);
-#if ARDUINO
-    static void add_pci(U16 pin, U16 xt);
-    static void enable_timer(U16 f);
-    static void enable_pci(U16 f);
-#else // !ARDUINO (mocked interrupt handlers)
-    static void add_pci(U16 pin, U16 xt) {}
-    static void enable_timer(U16 f)      {}
-    static void enable_pci(U16 f)           {}
-#endif
-};
 #endif //__SRC_NANOFORTH_INTR_H
