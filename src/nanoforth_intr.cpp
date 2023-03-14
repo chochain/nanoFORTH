7e1c2c76e34dbe40de48eb6b72e155ed759ee4b5
Chochain Lee <chochain@yahoo.com>
2023-03-14 19:36:57 +0800

    ~/src/n4_intr.cpp reduce timer period to 10ms; update isr()

--- src/nanoforth_intr.cpp
+++ src/nanoforth_intr.cpp
@@ -12,7 +12,8 @@ U8  t_idx  { 0 };          ///< timer ISR index
 U16 p_xt[] { 0, 0, 0 };    ///< pin change interrupt vectors
 U16 t_xt[8]; 		       ///< timer interrupt vectors
 U16 t_max[8]; 		       ///< timer CTC top value
-
+    
+volatile U8  _hits { 0 };  ///< interrupt flags
 volatile U8  t_hit { 0 };  ///< 8-bit for 8 timer ISR
 volatile U8  p_hit { 0 };  ///< pin change interrupt (PORT-B,C,D)
 volatile U16 t_cnt[8];     ///< timer CTC counters
@@ -25,32 +26,44 @@ void reset() {
 ///
 ///> fetch interrupt hit flags
 ///
-U16 hits() {
+U16 isr() {
     CLI();
-    U16 hx = (p_hit << 8) | t_hit;   // capture interrupt flags
-    p_hit = t_hit = 0;
+    if (!_hits) {
+        _hits = (p_hit << 8) | t_hit;   // capture interrupt flags
+        t_idx = t_hit = p_hit = 0;
+    }
     SEI();
-    return hx;
+    if (_hits) {
+        U8 hx = _hits & 0xff;
+        for (int i=0, t=1; hx && i<t_idx; i++, t<<=1, hx>>=1) {
+            if (_hits & t) { _hits &= ~t; return t_xt[i]; }
+        }
+        hx = _hits >> 8;
+        for (int i=0, t=0x100; hx && i<3; i++, t<<=1, hx>>=1) {
+            if (_hits & t) { _hits &= ~t; return p_xt[i]; }
+        }
+    }
+    return 0;
 }
-void add_timer(U16 n, U16 xt) {
+void add_tmisr(U16 n, U16 xt) {
     if (xt==0 || t_idx > 7) return;  // range check
 
     CLI();
     t_xt[t_idx]  = xt;               // ISR xt
     t_cnt[t_idx] = 0;                // init counter
-    t_max[t_idx] = n;                // period (in 0.1s)
+    t_max[t_idx] = n;                // period (in 10ms)
     t_idx++;
     SEI();
 }
 #if !ARDUINO
-void add_pci(U16 p, U16 xt) {}       // mocked functions for x86
-void enable_pci(U16 f)      {}
-void enable_timer(U16 f)    {}
+void add_pcisr(U16 p, U16 xt) {}     // mocked functions for x86
+void enable_pci(U16 f)        {}
+void enable_timer(U16 f)      {}
 #else  // ARDUINO
 ///
 ///@name N4Intr static variables
 ///@{
-void add_pci(U16 p, U16 xt) {
+void add_pcisr(U16 p, U16 xt) {
     if (xt==0) return;               // range check
     CLI();
     if (p < 8)       {
@@ -79,19 +92,17 @@ void enable_pci(U16 f) {
 }
 void enable_timer(U16 f) {
     CLI();
-    pinMode(7, OUTPUT);                         // DEBUG: timer2 interrupt enable/disable
-
     TCCR2A = TCCR2B = TCNT2 = 0;                // reset counter
     if (f) {
         TCCR2A = _BV(WGM21);                    // Set CTC mode
         TCCR2B = _BV(CS22)|_BV(CS21);           // prescaler 256 (16000000 / 256) = 62500Hz = 16us
-        OCR2A  = 249;                           // 250Hz = 4ms, (250 - 1, must < 256)
+//        OCR2A  = 249;                         // 250KHz = 4ms, (250 - 1, must < 256)
+        OCR2A  = 124;                           // 500KHz = 2ms, (125 - 1, must < 256)
         TIMSK2 |= _BV(OCIE2A);                  // enable timer2 compare interrupt
         digitalWrite(7, HIGH);                  // DEBUG: timer2 enabled
     }
     else {
         TIMSK2 &= _BV(OCIE2A);                  // disable timer2 compare interrupt
-        digitalWrite(7, LOW);                   // DEBUG: timer disabled
     }
     SEI();
 }
@@ -104,7 +115,8 @@ void enable_timer(U16 f) {
 #if ARDUINO
 ISR(TIMER2_COMPA_vect) {
     volatile static int cnt = 0;
-    if (++cnt < 25) return;                      // 25 * 4ms = 100ms
+//    if (++cnt < 25) return;                      // 25 * 4ms = 100ms (allows ~4K ops, max 3200 sec)
+    if (++cnt < 5) return;                         // 5 * 2ms = 10ms (allows ~400 ops, max 320 sec)
     cnt = 0;
     for (U8 i=0, b=1; i < N4Intr::t_idx; i++, b<<=1) {
         digitalWrite(7, digitalRead(7) ? LOW : HIGH);  // DEBUG: ISR called
