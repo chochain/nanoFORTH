eb65f6e66fabd02f5c1cb9f2c79d17c0159c394e
gnii <chochain@yahoo.com>
2023-03-17 18:57:48 -0700

    ~/example/3_bluetooth update for embedded

--- examples/3_bluetooth/3_bluetooth.ino
+++ examples/3_bluetooth/3_bluetooth.ino
@@ -1,5 +1,5 @@
 /**
- * @file examples/2_bluetooth/2_bluetooth.ino
+ * @file
  * @brief nanoFORTH - wireless with Bluetooth HC-05
  *
  * Assume you have a HC-05 hooked-up on Pin 8 and 9
@@ -22,34 +22,18 @@
 #include <nanoforth.h>
 
 AltSoftSerial bt;                   ///< default: RX on pin 8, TX on pin 9
-NanoForth     n4;                   ///< create NanoForth instance
 
 void setup() {
+    Serial.begin(115200);           ///< init Serial IO
     while (!Serial);
-    Serial.begin(115200);           ///< init Serial IO, make sure it is set to 'Both BL & CR' to capture input
 
     bt.begin(9600);                 ///< setup Bluetooth serial device at 9600 baud
     delay(1000);                    ///< wait for a while, for it to stable
 
-    pinMode(LED_BUILTIN, OUTPUT);
-    
-    n4.add_task(blink13);           ///< add the blink task to NanoForth task manager
-    if (n4.begin(bt)) {             ///< setup Bluetooth as nanoFORTH console
-        Serial.print(F("ERROR: memory allocation failed!"));
-    }
+    n4_setup("", 1, bt);            ///< add the blink task to NanoForth task manager
 }
 
 void loop() {
-    n4.exec();                      // execute command line from BT serial console
+    n4_run();                       // execute command line from BT serial console
 }
-///
-/// User defined task - blinks built-in LED
-///
-N4_TASK(blink13) {                  ///< create blinking task (i.e. built-in LED on pin 13)
-    digitalWrite(LED_BUILTIN, HIGH);
-    N4_DELAY(500);
-    digitalWrite(LED_BUILTIN, LOW);
-    N4_DELAY(500);
-} N4_END;
-
 
