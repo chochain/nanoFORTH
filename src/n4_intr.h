28b0cd81ff2bd94b40f1555c5f36fdbc2da2cf8c
gnii <chochain@yahoo.com>
2023-05-12 11:27:52 -0700

    ~/src/n4_intr.h no extern needed

--- src/n4_intr.h
+++ src/n4_intr.h
@@ -19,10 +19,6 @@
 #endif // ARDUINO
 
 namespace N4Intr {
-    extern U8   t_idx;             ///< max slot# of timer interrupt
-    extern U16  p_xt[3];           ///< pin change ISR
-    extern U16  t_xt[8];           ///< timer ISR
-
     void reset();                  ///< reset interrupts
     U16  isr();                    ///< fetch interrupt service routines
 
